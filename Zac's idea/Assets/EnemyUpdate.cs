bd67af8ed9b6d4437e927e35d7c88bea3f9dacb8
zgoad1 <zqgoad@gmail.com>
2018-02-12 18:26:33 -0600

    Add enemy movement

--- Zac's idea/Assets/EnemyUpdate.cs
+++ Zac's idea/Assets/EnemyUpdate.cs
@@ -11,27 +11,58 @@ public class EnemyUpdate : MonoBehaviour {
 
     public NavMeshAgent agent;
 
-    private Vector3 destination;
+    private Vector3 destination = Vector3.zero;
     private int waitFrames;
-    private int waitMin = 20;
-    private int waitMax = 240;
+    private int waitMin = 60;
+    private int waitMax = 300;
+    private float distance;
+    private float distanceMin = 10f;
+    private float distanceMax = 25f;
+    private string state = "wait";
 
-    void setWaitFrames() {
+    void SetWaitFrames() {
         waitFrames = Random.Range(waitMin, waitMax);
+        Debug.Log("setting waitframes to " + waitFrames);
     }
 
-    void setDestination() {
+    void SetDestination() {
+        distance = Random.Range(distanceMin, distanceMax);
+        Vector2 randomMove;
 
+        // move a random direction in a radius of this distance
+        randomMove = Random.insideUnitCircle * distance;
+        destination.x += randomMove.x;
+        destination.z += randomMove.y;
+        Debug.Log("Moving");
+
+        agent.SetDestination(destination);
+
+        Debug.Log("setting destination to " + destination);
     }
 
-	// Use this for initialization
-	void Start () {
-        setWaitFrames();
-        setDestination();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        SetWaitFrames();
+        destination = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(state == "wait") {
+            if(waitFrames <= 0) {
+                SetDestination();
+                SetWaitFrames();
+                state = "move";
+            }
+
+            waitFrames--;
+        } else {
+            // if we're basically at the destination, change state to wait
+            if(transform.position.x == destination.x && transform.position.z == destination.z) {
+                Debug.Log("Stopped, changing to wait");
+                state = "wait";
+            }
+        }
+    }
 }
